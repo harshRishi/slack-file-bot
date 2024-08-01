039e5e7cbbff498a19c204a4dec1fd99766c26e9
Harsh <harshuniverse000@gmail.com>
2024-08-01 19:45:03 +0530

    slack bot completed

--- main.go
+++ main.go
@@ -5,16 +5,33 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
 func main() {
-	godotenv.Load(".env")
-	sklId := os.Getenv("SLACK_BOT_TOKEN")
-	chId := os.Getenv("CHANNEL_ID")
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("Error loading .env file: %s\n", err)
+		os.Exit(1)
+	}
 
-	fmt.Println("My Creds: ", sklId, chId)
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	channelID := os.Getenv("CHANNEL_ID")
+	filePath := "./test.pdf"
 
-	// api := slack.New()
-	// channelArr := []string{}
-	// fileArr := []string{""}
+	client := slack.New(token)
+
+	params := slack.FileUploadParameters{
+		Channels:       []string{channelID},
+		InitialComment: "Here's my file : Smile:",
+		File:           filePath,
+	}
+
+	uploadedFile, err := client.UploadFile(params)
+	if err != nil {
+		fmt.Printf("Error uploading file: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Name: %s, URL: %s\n", uploadedFile.Name, uploadedFile.URL)
 }
